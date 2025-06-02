bf0ff74a344934d6764b1df58cbc7cfec1886ec4
Cesar Trujillo <rayzler.404@gmail.com>
2025-06-01 18:47:51 -0600

    update and get daily challenge

---
+++ prisma/migrations/20250602004234_remove_emoji_id/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `emoji_id` on the `daily_challenges` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "daily_challenges" DROP COLUMN "emoji_id";
---
+++ src/lib/dateUtils.ts
@@ -0,0 +1,6 @@
+export const getUTCDate = (): Date => {
+  const now = new Date();
+  return new Date(
+    Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate())
+  );
+};
