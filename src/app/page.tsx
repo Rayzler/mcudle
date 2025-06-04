0988ed10f9acf8cd20ca6d8cddd9ed306c02fbb2
Cesar Trujillo <rayzler.404@gmail.com>
2025-06-04 10:14:37 -0600

    home page view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,30 @@
+import MainButton from "@/components/main-button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[1fr_20px] items-center justify-items-center min-h-screen p-8 pt-16 gap-8">
+      <main className="flex flex-col gap-5 justify-start items-center w-full h-full">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
+          src="/images/ui/logo.png"
+          alt="MCU-DLE logo"
+          width={275}
+          height={98}
+          className="box-reflect mb-6"
           priority
         />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <h1 className="text-2xl">Guess the daily Hero! or Villain?</h1>
+        <div className="flex flex-col gap-4 w-full items-center">
+          <MainButton label="Classic" description="Guess the character with clues" />
+          <MainButton label="Movie" description="Guess the movie from a list of frames" />
+          <MainButton label="Image" description="Guess the character from an image section" />
+          <MainButton label="Quote" description="Guess who said the quote" />
+          <MainButton label="Item" description="Guess the owner of the item" />
+          <MainButton label="Emoji" description="Guess the character with emojis" />
         </div>
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+      {/* TODO: Footer */}
+      <footer className="flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
---
+++ src/fonts.ts
@@ -0,0 +1,11 @@
+import { Share_Tech, Kdam_Thmor_Pro } from "next/font/google";
+
+export const shareTech = Share_Tech({
+  weight: "400",
+  subsets: ["latin"]
+});
+
+export const kdamThmorPro = Kdam_Thmor_Pro({
+  weight: "400",
+  subsets: ["latin"]
+});
